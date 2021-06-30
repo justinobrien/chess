685e7544305ee03e1a44f5c4f3e63420b54b3d3a
justinobrien <obrien.justin1008@gmail.com>
2021-06-30 16:13:18 -0700

    simple grid css layout

--- src/chess/components/gameBoard/GameBoard.jsx
+++ src/chess/components/gameBoard/GameBoard.jsx
@@ -1,10 +1,48 @@
 import React from 'react';
 import "./GameBoard.scss";
+const Board = () => {
+    // check clipboard for position data
+    //  button to do this manually
+    // flip board button
+    // 1-8 amd a-h
 
-const GameBoard = () => {
+    const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+    const verticalAxis =   ["1", "2", "3", "4", "5", "6", "7", "8"];
+
+    
+    let board = [];
+    let squarecolor = true;
+
+    // initialize board
+    horizontalAxis.forEach(i => {
+        verticalAxis.forEach(j => {
+            let boardSquare = {
+                color: "white", //refactor to 0 or 1?
+                location: "", // ex: a6, b8
+            };
+            boardSquare.location = i + j;
+            if (squarecolor) {
+                boardSquare.color = "white";
+                squarecolor = !squarecolor;
+            } else {
+                boardSquare.color = "black";
+                squarecolor = !squarecolor;
+            }
+            board.push(boardSquare);
+        });
+        // swap start color for each row
+        squarecolor = !squarecolor;
+    });
+
+    console.warn(board);
+
     return (
-        <div className="game-board">Board</div>
+        <div className="game-board">
+            {board.map(square => (
+                <div className={square.color}> {square.location} </div>
+            ))}
+        </div>
     );
-};
+}
 
-export default GameBoard;
+export default Board;
