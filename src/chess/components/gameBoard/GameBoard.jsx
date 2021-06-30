54b8517d7791b66000197cc8382de9a56dd29f77
justinobrien <obrien.justin1008@gmail.com>
2021-06-30 14:36:07 -0700

    initial project setup

---
+++ src/chess/components/gameBoard/GameBoard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./GameBoard.scss";
+
+const GameBoard = () => {
+    return (
+        <div className="game-board">Board</div>
+    );
+};
+
+export default GameBoard;
