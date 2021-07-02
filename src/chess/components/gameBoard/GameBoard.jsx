27b2da1eebb87bb15b9100280ebf55d88d88a6b9
justinobrien <obrien.justin1008@gmail.com>
2021-07-02 09:04:28 -0700

    change Board to a class component

--- src/chess/components/gameBoard/GameBoard.jsx
+++ src/chess/components/gameBoard/GameBoard.jsx
@@ -1,55 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./GameBoard.scss";
-const Board = () => {
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
     // check clipboard for position data
     //  button to do this manually
     // flip board button
     // 1-8 amd a-h
 
-    const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
-    const verticalAxis =   ["1", "2", "3", "4", "5", "6", "7", "8"];
+    horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+    verticalAxis =   ["1", "2", "3", "4", "5", "6", "7", "8"];
 
-    
-    let board = [];
-    let squarecolor = false;
+    board = [];
 
     // initialize board
-    verticalAxis.reverse().forEach(i => {
-        board.push({location: i, color: "clear"});
-        horizontalAxis.forEach(j => {
-            let boardSquare = {
-                color: "white",
-                location: "", // ex: a6, b8
-                piece: ""
-            };
-            boardSquare.location = i + j;
-            if (squarecolor) {
-                boardSquare.color = "white";
-                squarecolor = !squarecolor;
-            } else {
-                boardSquare.color = "black";
-                squarecolor = !squarecolor;
-            }
-            board.push(boardSquare);
+    initializeBoard = () => {
+        let squarecolor = false;
+
+        this.verticalAxis.reverse().forEach(i => {
+            this.board.push({location: i, color: "clear"});
+            this.horizontalAxis.forEach(j => {
+                let boardSquare = {
+                    color: "white",
+                    location: "", // ex: a6, b8
+                    piece: ""
+                };
+                boardSquare.location = i + j;
+                if (squarecolor) {
+                    boardSquare.color = "white";
+                    squarecolor = !squarecolor;
+                } else {
+                    boardSquare.color = "black";
+                    squarecolor = !squarecolor;
+                }
+                this.board.push(boardSquare);
+            });
+            // swap start color for each row
+            squarecolor = !squarecolor;
         });
-        // swap start color for each row
-        squarecolor = !squarecolor;
-    });
+    }
 
-    console.warn(board);
+    // console.warn(board);
 
-    return (
-        <div className="game-board">
-            {board.map(square => (
-                <div className={square.color}> {square.location} </div>
-            ))}
-            {/* empty div for grid layout */}
-            <div></div>
-            {horizontalAxis.map( label =>
-                <div className="clear"> {label} </div>
-            )}
-        </div>
-    );
+    render() {
+        return (
+            <div className="game-board">
+                {this.board.map(square => (
+                    <div className={square.color}> {square.location} </div>
+                ))}
+                {/* empty div for grid layout */}
+                <div></div>
+                {this.horizontalAxis.map( label =>
+                    <div className="clear"> {label} </div>
+                )}
+            </div>
+        );
+    }
 }
 
 export default Board;
